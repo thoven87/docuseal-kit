7defdce370d992b8c6d3e9a20a1290daf620a63f
Stevenson Michel <130018170+thoven87@users.noreply.github.com>
2025-05-12 22:46:35 -0400

    Update AsyncHTTClient to version 1.26.0 (#12)

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.25.2"),
+        .package(url: "https://github.com/swift-server/async-http-client.git", from: "1.26.0"),
         .package(url: "https://github.com/vapor/jwt-kit.git", from: "5.1.2"),
         .package(url: "https://github.com/apple/swift-log.git", from: "1.6.3"),
     ],
